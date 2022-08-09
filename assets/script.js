77c6a3d3b121064b472aebc79d1c05dac1acae86
Ben Hobson <benhobson@Bens-MacBook-Pro.local>
2022-08-08 17:01:16 -0700

    class change works

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,31 @@
+//Today's Date
 var currentDate = moment().format("dddd, MMMM Do YYYY");
-$("#currentDay").text(currentDate);
+$("#currentDate").text(currentDate);
+
+function checkHour(){
+    var currentHour = moment().hour();
+
+   $(".time-block").each(function(){
+     
+    var hour = parseInt($(this).attr("id"));
+
+    if(hour < currentHour){
+        $(this).addClass("past");
+    } else if(hour === currentHour){
+        $(this).addClass("present");
+    } else {
+        $(this).addClass("future");
+    }
+
+
+
+   })  
+}
+checkHour();
+
+
+
+
+
+
+
