852ab518f3ce4e03d085d552091859275f17b2fb
Ben Hobson <benhobson@Bens-MacBook-Pro.local>
2022-08-08 17:55:03 -0700

    button

--- assets/script.js
+++ assets/script.js
@@ -2,26 +2,42 @@
 var currentDate = moment().format("dddd, MMMM Do YYYY");
 $("#currentDate").text(currentDate);
 
-function checkHour(){
-    var currentHour = moment().hour();
+$(document).ready(function() {
 
-   $(".time-block").each(function(){
-     
-    var hour = parseInt($(this).attr("id"));
+    function checkHour() {
+        var currentHour = moment().hour();
 
-    if(hour < currentHour){
-        $(this).addClass("past");
-    } else if(hour === currentHour){
-        $(this).addClass("present");
-    } else {
-        $(this).addClass("future");
+        $(".time-block").each(function () {
+
+            var hour = parseInt($(this).attr("id"));
+
+            if (hour < currentHour) {
+                $(this).removeClass("present");
+                $(this).removeClass("future");
+                $(this).addClass("past");
+            } else if (hour === currentHour) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            } else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+            }
+        })
     }
 
+    $(".saveBtn").on("click", function () {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+        localStorage.setItem(time, text);
+    })
 
 
-   })  
-}
-checkHour();
+    checkHour();
+    setInterval(checkHour, 60000);
+
+})
 
 
 
